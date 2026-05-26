f1d2b5791541b13d7527d8409bbc04fd4a06af5e
mikagouzee <michel.gouzee@hotmail.com>
2026-05-26 10:20:53 +0200

    made go equip more dynamic

--- routines/equip.py
+++ routines/equip.py
@@ -7,25 +7,24 @@ from townhall import town_hall
 
 async def go_equip(context, item_code, action:ActionController,db:db_controller, slot="weapon", quantity=1):
   slot_name = f'{slot}_slot'
-  if context.current_hero.getattr(slot_name) == item_code:
+  if getattr(context.current_hero, slot_name) == item_code:
     return context
 
-  if check_quantity_in_bag(context.current_hero.inventory, item_code):
+  if check_quantity_in_bag(context.current_hero.inventory, item_code) >= quantity:
     print(f'{context.current_hero.name} found {item_code} !.')
     context = await action.hero.equip(context, item_code, slot=slot, qtty=quantity)
   else:
-    dest = await db.get_closest_map(context, content_code='bank', content_type='bank')
-    if dest and not check_location(context.current_hero, dest.x, dest.y):
-      context = await action.hero.move(context, dest.x, dest.y)  
-    context = await action.bank.withdraw(context, item_code, quantity)
-
-    if check_quantity_in_bag(context.current_hero.inventory, item_code):
-      context = await action.hero.equip(context, item_code, slot=slot, qtty=quantity)
+    if check_quantity_in_bag(await action.bank.get_bank_inventory(), item_code) >= quantity:
+      dest = await db.get_closest_map(context, content_code='bank', content_type='bank')
+      if dest and not check_location(context.current_hero, dest.x, dest.y):
+        context = await action.hero.move(context, dest.x, dest.y)  
+        context = await action.bank.withdraw(context, item_code, quantity)
+        context = await action.hero.equip(context, item_code, slot=slot, qtty=quantity)  
     else:
         await town_hall.report_need(
                     quest_type="craft",
-                    target="small_health_potion", 
-                    quantity=200, 
+                    target=item_code, 
+                    quantity=quantity*2, 
                     priority=99, 
                     requester=context.current_hero.name,
                     assigned_to = None)
